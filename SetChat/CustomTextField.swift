6e47ea68d79a7c3a097575c585654240a7306d93
Mehmet Kerim ÖZEK <kerimantik198@gmail.com>
2023-04-13 17:31:26 +0300

    refactor placeholderTextAlpha

--- SetChat/CustomTextField.swift
+++ SetChat/CustomTextField.swift
@@ -9,17 +9,44 @@ import UIKit
 
 class CustomTextField: UITextField {
     
+    // Define the placeholder text color and alpha for when the field is empty
+    private let emptyPlaceholderColor = UIColor.white.withAlphaComponent(0.4)
+    
     init(placeholder: String) {
         super.init(frame: .zero)
         
-        attributedPlaceholder = NSMutableAttributedString(string: placeholder, attributes:[.foregroundColor: UIColor.white])
+        attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [.foregroundColor: emptyPlaceholderColor])
         borderStyle = .none
         textColor = .white
+        
+        addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func drawPlaceholder(in rect: CGRect) {
+        let placeholderRect = CGRect(x: 0, y: (bounds.size.height - font!.lineHeight) / 2, width: bounds.size.width, height: bounds.size.height)
+        
+        // Set the placeholder color and alpha based on whether the text field is the first responder or if it contains any text.
+        let placeholderColor = UIColor.white.withAlphaComponent(isFirstResponder || !(text?.isEmpty ?? true) ? 1.0 : 0.4)
+        
+        // Set the attributes for the attributed placeholder text.
+        let _: [NSAttributedString.Key: Any] = [.foregroundColor: placeholderColor]
+        
+        // Draw the attributed placeholder text using the set attributes.
+        if let attributedPlaceholder = attributedPlaceholder {
+            attributedPlaceholder.draw(in: placeholderRect)
+            attributedPlaceholder.draw(with: placeholderRect, options: [.usesLineFragmentOrigin], context: nil)
+        }
+    }
+
+
     
+    @objc private func textFieldDidChange() {
+        // Redraw the placeholder text when the text changes
+        setNeedsDisplay()
+    }
 }
+
